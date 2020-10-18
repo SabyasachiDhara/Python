8db9e9b552f8da793709f43af676302609aef0d4
SabyasachiDhara <you@example.com>
2020-10-18 09:42:37 +0530

    2nd commit v1.1

--- Profiling.py
+++ Profiling.py
@@ -1,12 +1,26 @@
-check=('Sabyasachi Dhara','Pallab Mahapatra','Jit bhattacharya','Suman Adhikary','Sumit Nayek')
+import xlrd
+import openpyxl
 
-name = input('What is your name :: ')
+check={'admin'}
+
+wb = xlrd.open_workbook(r'C:\Users\sabya\Documents\database.xlsx')
+
+sheet = wb.sheet_by_index(0)
+sheet.cell_value(0,0)
+
+for i in range(sheet.nrows):
+    imp=sheet.cell_value(i,0)
+    check.add(imp)
+
+l = len(check)
+p='A'+str(l)
+
+name = input('\nWhat is your name :: ')
 
 def welcome(n):
     if n in check:
-        print ('Welcome Home..')
+        print ('\nWelcome Home..')
     else:
-        print ('!!!!...Invalid user...!!!!')
-        print ('Please Sign in First, Then Try..')
+        print ('\n\n!!!!...Invalid user...!!!!')
 
 welcome(name)
